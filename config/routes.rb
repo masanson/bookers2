53ec18f21efdb6692d944ff6c5d9b79a8bc0426d
masanson <138214683+masanson@users.noreply.github.com>
2023-07-31 10:12:05 +0000

    [Update] ページ作成2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
   root to: 'homes#top'
+  resources :users, only: [:show, :edit,]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
